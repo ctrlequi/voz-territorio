c5dd693d6efb23df572d28187f8cf68ec8dc8051
ralex <ralex.rdz@gmail.com>
2019-05-28 09:32:41 -0500

    Updates

---
+++ precache-manifest.e1fca28ebb50e267be8340b180a05a97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8074fa9099900ad725ed4467489159e9",
+    "url": "/voz-territorio/index.html"
+  },
+  {
+    "revision": "baa8bd0fc77620cbcece",
+    "url": "/voz-territorio/static/css/main.d4709c7c.chunk.css"
+  },
+  {
+    "revision": "d2c34891f8b0d862627d",
+    "url": "/voz-territorio/static/js/2.88273e26.chunk.js"
+  },
+  {
+    "revision": "baa8bd0fc77620cbcece",
+    "url": "/voz-territorio/static/js/main.7f5cf346.chunk.js"
+  },
+  {
+    "revision": "adc4a36802fe19f293bc",
+    "url": "/voz-territorio/static/js/runtime~main.26685c72.js"
+  }
+]);
