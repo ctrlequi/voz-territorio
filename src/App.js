2af22b2bc76e5ef57616089358f5c7d73fcbdbdd
ralex <ralex.rdz@gmail.com>
2019-05-28 10:10:14 -0500

    published

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-import {Switch, Route, Link} from 'react-router-dom';
+import {HashRouter, Switch, Route, Link} from 'react-router-dom';
 
 // import ListaProductos from "./components/ListaProductos";
 // import FormularioProductos from "./components/FormularioProducto";
@@ -13,16 +13,18 @@ import Map from "./components/Map/Map";
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <HashRouter basename="/">
+        <div className="App">
         <header>
-          <Link to="/"> Prodcutos </Link>
-          <Link to="/new-product"> + Producto </Link>
+          <Link to="/"> Home </Link>
+          <Link to="/map"> Map </Link>
         </header>
         <Switch>
           <Route exact path="/" component={Home}/>
           <Route exact path="/map" component={Map}/>
         </Switch>
       </div>
+      </HashRouter>
     )
   }
 }
