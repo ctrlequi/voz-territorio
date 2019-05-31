aa8f7a8ca07e7d14be0bddb35d50b71dc5f2182b
RaquelAM <raquelaguilar@ciencias.unam.mx>
2019-05-31 01:06:43 -0500

    more data

--- src/data/observations.js
+++ src/data/observations.js
@@ -10,6 +10,18 @@ const observations = [
         imageUrl: 'http://1.bp.blogspot.com/-kesHIYYUBqk/TnhfgS_3ofI/AAAAAAAAFso/AdSpsDicrXg/s1600/FoolMoon.jpg'
       }
     ]
+  },
+  {
+    title: 'Amanecer',
+    lat: 18.977605,
+    lng: -99.183547,
+    id: 124,
+    contents: [
+      {
+        'type': 'image',
+        imageUrl: 'https://res.cloudinary.com/lavozdelterritorio/image/upload/v1559273122/Plataforma/DSC_0222_jmmd8a.jpg'
+      }
+    ]
   }
 ]
 
