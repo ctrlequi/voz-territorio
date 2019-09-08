c60f6df70a1a0ede154c9cf6d47b67c4c00b5204
ralex <ralex.rdz@gmail.com>
2019-09-07 22:13:34 -0500

    Updates

---
+++ precache-manifest.900f2e45ea316319f17dab5753bfed20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb904538dbf1b766b59afd63c281b83d",
+    "url": "/voz-territorio/index.html"
+  },
+  {
+    "revision": "8237ce1e108c17d4dc3c",
+    "url": "/voz-territorio/static/css/2.f5f0c74c.chunk.css"
+  },
+  {
+    "revision": "fe130b8e251141f80432",
+    "url": "/voz-territorio/static/css/main.a6315618.chunk.css"
+  },
+  {
+    "revision": "8237ce1e108c17d4dc3c",
+    "url": "/voz-territorio/static/js/2.384a9753.chunk.js"
+  },
+  {
+    "revision": "fe130b8e251141f80432",
+    "url": "/voz-territorio/static/js/main.6ed9bc30.chunk.js"
+  },
+  {
+    "revision": "adc4a36802fe19f293bc",
+    "url": "/voz-territorio/static/js/runtime~main.26685c72.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/voz-territorio/precache-manifest.136597d3834b40debf47009047e36679.js"
+  "/voz-territorio/precache-manifest.900f2e45ea316319f17dab5753bfed20.js"
 );
 
 self.addEventListener('message', (event) => {
